20f60d8fe1fb70741437c6972131dd8c7473a2f2
ArdaKorkmaz0 <korkmazarda4455@gmail.com>
2026-05-25 22:41:41 +0300

    Add timezone setting to user profile

--- analyzer/forms.py
+++ analyzer/forms.py
@@ -6,8 +6,14 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['chesscom_username', 'lichess_username']
+        fields = ['chesscom_username', 'lichess_username', 'timezone_offset']
         labels = {
             'chesscom_username': 'Chess.com username',
-            'lichess_username': 'Lichess username',
+            'lichess_username':  'Lichess username',
+            'timezone_offset':   'Your timezone (hours from UTC)',
+        }
+        help_texts = {
+            'timezone_offset':
+                'Used to display tilt analysis times in your local hours. '
+                '0 = UTC, 3 = Turkey, -5 = US Eastern.',
         }
--- analyzer/models.py
+++ analyzer/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -6,6 +7,13 @@ class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     chesscom_username = models.CharField(max_length=80, blank=True)
     lichess_username = models.CharField(max_length=80, blank=True)
+    # Hours offset from UTC. Used to group games by hour-of-day in tilt analysis.
+    # 3 = Turkey (UTC+3), 0 = UTC, -5 = US Eastern, etc. Range covers all real zones.
+    timezone_offset = models.IntegerField(
+        default=3,
+        validators=[MinValueValidator(-12), MaxValueValidator(14)],
+        help_text="Hours offset from UTC. Used to localize 'hour of day' stats.",
+    )
 
     def __str__(self):
         return f"{self.user.username} profile"
